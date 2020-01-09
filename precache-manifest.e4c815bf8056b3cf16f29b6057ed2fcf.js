20890cb991b104dbd7dadaf09445eb0e8ec4626d
Oles Marola <oles_pw@ukr.net>
2020-01-09 23:34:36 +0200

    Updates

---
+++ precache-manifest.e4c815bf8056b3cf16f29b6057ed2fcf.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "935eea81609eefe3c8d1b6ef2ddd67b0",
+    "url": "./index.html"
+  },
+  {
+    "revision": "2ee842747765db693095",
+    "url": "./static/css/main.6dca6589.chunk.css"
+  },
+  {
+    "revision": "5661fc9b355930be0d6e",
+    "url": "./static/js/2.29c6f01d.chunk.js"
+  },
+  {
+    "revision": "2ee842747765db693095",
+    "url": "./static/js/main.9ff53b67.chunk.js"
+  },
+  {
+    "revision": "8c97409f0ee389fe75da",
+    "url": "./static/js/runtime~main.d653cc00.js"
+  },
+  {
+    "revision": "cb2bd0213cb82b6309b3d20f43e81e41",
+    "url": "./static/media/Subtle-Prism.cb2bd021.svg"
+  }
+]);
